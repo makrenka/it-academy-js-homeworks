da7907dd3cf6747b9fe1c62688b8995515cd3ec8
makrenka <smokrenko79@gmail.com>
2022-09-21 20:16:52 +0300

    add triangle and grid

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 import "./main.scss";
 
+// Треугольник в цикле
+// let a = '';
+// for (i = 0; i < 7; i++) {
+//     a += '#';
+//     console.log(a);
+// }
+
+// Решетка 8х8
+// for (i = 0; i < 8; i++) {
+//     if (i % 2 == 0) {
+//         a = '#';
+//         b = ' ';
+//     } else {
+//         a = ' ';
+//         b = '#';
+//     }
+//     let string = a + b;
+
+//     for (j = 0; j < 6; j++) {
+//         if (j % 2 == 0) {
+//             string += a;
+//         } else {
+//             string += b;
+//         }
+//     };
+
+//     console.log(string);
+// }
 
