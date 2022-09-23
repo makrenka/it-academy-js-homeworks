535dd1116537f50c7a007345f5dbaa15f59801fe
makrenka <smokrenko79@gmail.com>
2022-09-23 10:25:30 +0300

    edit anketa

--- src/index.js
+++ src/index.js
@@ -41,7 +41,6 @@ let firstName = prompt('Введите ваше имя', '');
 let patronymic = prompt('Введите ваше отчество', '');
 let age = prompt('Ваш возраст', '');
 let gender = confirm('Ваш пол - мужской?');
-let lineWrapper = '\n';
 
 function checkLastName() {
     if (lastName == '' || lastName == null) {
@@ -54,31 +53,30 @@ function checkLastName() {
     }
 };
 
-let ageInYears;
 let ageInDays;
 let ageAfterFiveYears;
 
-function checkAge() {
+function checkAge () {
     if (age < 1 || age > 120) {
         do {
             age = prompt('Введите правильный возраст', '');
         } while (age < 1 || age > 120);
-        ageInYears = 'ваш возраст в годах: ' + age;
-        ageInDays = 'ваш возраст в днях: ' + age * 365;
+        ageInDays = 'ваш возраст в днях: ' + age * 365;  
         ageAfterFiveYears = 'через 5 лет вам будет: ' + (Number(age) + 5);
+        return 'ваш возраст в годах: ' + age;
     } else {
-        ageInYears = 'ваш возраст в годах: ' + age;
-        ageInDays = 'ваш возраст в днях: ' + age * 365;
+        ageInDays = 'ваш возраст в днях: ' + age * 365;  
         ageAfterFiveYears = 'через 5 лет вам будет: ' + (Number(age) + 5);
+        return 'ваш возраст в годах: ' + age;        
     }
 };
 
-let genderUser = 'ваш пол: ' + gender;
+let genderUser = (gender == true) ? 'ваш пол: мужской' : 'ваш пол: женский';
 let penny = (age < 62) ? 'вы на пенсии: нет' : 'вы на пенсии: да';
 
 function showAnketa() {
-    return checkLastName() + lineWrapper + checkAge() + lineWrapper + ageInDays +
-        lineWrapper + ageAfterFiveYears + lineWrapper + genderUser + lineWrapper + penny;
+    return checkLastName() + '\n' + checkAge() + '\n' + ageInDays +
+    '\n' + ageAfterFiveYears + '\n' + genderUser + '\n' + penny;
 }
 
 alert(showAnketa());
