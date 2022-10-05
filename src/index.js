1d66a93c9a73943a667b1f01c86381acba3ddfe6
makrenka <smokrenko79@gmail.com>
2022-10-05 14:48:52 +0300

    add file hw-3

--- src/index.js
+++ src/index.js
@@ -1,17 +1,3 @@
 import "./main.scss";
+import "./modules/hw-3";
 
-const numberArray = [ 5, 7, 
-    [ 4, [2], 8, [1,3], 2 ], 
-    [ 9, [] ], 
-    1, 8
-  ];
-
-  function treeSum(ar) {
-    let sum = 0;
-    for (let item of ar) {
-         sum += Array.isArray(item) ? treeSum(item) : item;
-    }
-    return sum;
-  }
-
-console.log(treeSum(numberArray));
