164ddcacc2cc1ed625b05a75e422dbfaf7cf561f
makrenka <smokrenko79@gmail.com>
2022-10-05 15:09:33 +0300

    edit index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import "./main.scss";
-import "./modules/hw-1";
-import "./modules/hw-2";
-import "./modules/hw-3";
+// import "./modules/hw-1";
+// import "./modules/hw-2";
+// import "./modules/hw-3";
 
 
 
