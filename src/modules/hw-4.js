d552a2bf13a95ba7dbdd18b64847228ba8f20e1a
makrenka <smokrenko79@gmail.com>
2022-10-05 20:30:34 +0300

    add Colon between odd numbers

--- src/modules/hw-4.js
+++ src/modules/hw-4.js
@@ -10,6 +10,7 @@
 // console.log(arr1);
 
 
+
 // 2. Convert array to string
 // let vegetables = ['Капуста', 'Репа', 'Редиска', 'Морковка'];
 
@@ -18,3 +19,28 @@
 
 // console.log(str1); 
 // console.log(str2);
+
+
+
+// 3. Colon between odd numbers
+// const num = prompt('Введите число', 55);
+
+// function colonOdd (num) {
+//   let arr = num.split('').map((item) => +item);    
+//   let arrMod = [arr[0]];
+
+//   for (let i = 1; i < arr.length; i++) {        
+//     if ((arr[i-1] % 2 !== 0) && (arr[i] % 2 !== 0)) {
+//       arrMod.push(':', arr[i]);
+//     } else {
+//       arrMod.push(arr[i]);
+//     }    
+//   }
+
+//   return arrMod.join('');  
+// }
+
+// console.log(colonOdd (num));
+
+
+
