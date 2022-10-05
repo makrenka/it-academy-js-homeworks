9259a34bcbaf806931917aa9e59771036b459b7b
makrenka <smokrenko79@gmail.com>
2022-10-05 16:48:05 +0300

    add convert array

--- src/modules/hw-4.js
+++ src/modules/hw-4.js
@@ -1,12 +1,20 @@
-// 1. Cloning an array
-let vegetables = ['Капуста', 'Репа', 'Редиска', 'Морковка'];
+// 1. Clone array
+// let vegetables = ['Капуста', 'Репа', 'Редиска', 'Морковка'];
 
-function arrayClone(arr) {
-    return arr.slice();
-}
+// function arrayClone(arr) {
+//     return arr.slice();
+// }
 
-let arr1 = arrayClone(vegetables);
+// let arr1 = arrayClone(vegetables);
 
-console.log(arr1);
+// console.log(arr1);
 
 
+// 2. Convert array to string
+// let vegetables = ['Капуста', 'Репа', 'Редиска', 'Морковка'];
+
+// let str1 = vegetables.join();
+// let str2 = vegetables.toString();
+
+// console.log(str1); 
+// console.log(str2);
