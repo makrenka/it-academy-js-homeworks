f7f4e5bb56b4c7f1a7448461b0de79d0fd386e14
makrenka <smokrenko79@gmail.com>
2022-10-10 20:15:15 +0300

    create class Worker in ES5 and ES6

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import "./main.scss";
 // import "./modules/hw-1";
 // import "./modules/hw-2";
 // import "./modules/hw-3";
-import "./modules/hw-4";
-
+// import "./modules/hw-4";
+import "./modules/hw-5";
 
 
---
+++ src/modules/hw-5.js
@@ -0,0 +1,43 @@
+// Create class in ES5
+
+// function Worker(name, surname, rate, days) {
+//     this.name = name;
+//     this.surname = surname;
+//     this.rate = rate;
+//     this.days = days;
+//     this.getSalary = function() {
+//       return this.rate * this.days;
+//     }
+//   }
+  
+//   let worker = new Worker('Иван', 'Иванов', 10, 31);
+  
+//   console.log(worker.name);
+//   console.log(worker.surname);
+//   console.log(worker.rate);
+//   console.log(worker.days);
+//   console.log(worker.getSalary());
+
+
+// Create class in ES6
+
+// class Worker {
+//     constructor(name, surname, rate, days) {
+//       this.name = name;
+//       this.surname = surname;
+//       this.rate = rate;
+//       this.days = days;
+//     }
+  
+//     getSalary() {
+//       return this.rate * this.days;
+//     }
+//   }
+  
+//   let worker = new Worker('Иван', 'Иванов', 10, 31);
+  
+//   console.log(worker.name);
+//   console.log(worker.surname);
+//   console.log(worker.rate);
+//   console.log(worker.days);
+//   console.log(worker.getSalary());
